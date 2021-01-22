00d15f3d72cc74435d7acea082d85dfcc6adacec
HuemanTarget <joshbasche@live.com>
2021-01-22 11:44:02 -0800

    Initial Commit

---
+++ HitList/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HitList
+//
+//  Created by Joshua Basche on 1/22/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
